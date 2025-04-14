0a9c2ce718bc70acac8d787e8de23158c9c813ac
sccsc1342025 <sebastiancam711@gmail.com>
2025-04-14 02:30:55 +0000

    csc134_M5LAB2.cpp

---
+++ csc134_M5LAB2.cpp
@@ -0,0 +1,53 @@
+// CSC 134
+// M5LAB1
+// Sebastian Camacho
+// 04/13/2025
+
+#include <iostream>
+using namespace std;
+
+double getLength();
+double getWidth();
+double getArea(double length, double width);
+void displayData(double length, double width, double area);
+
+int main()
+{	
+   double length, width, area;  
+          
+   length = getLength();
+   
+   width = getWidth()
+   
+   area = getArea(length, width);
+   
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+double getLength() {
+    double length;
+    cout << "Enter rectangle length: " << endl;
+    cin >> length;
+    return length;
+}
+
+double getWidth() {
+    double width;
+    cout << "Enter rectangle width: " << endl;
+    cin >> width;
+    return width;
+}
+
+double getArea(double length, double width) {
+    return length * width;
+}
+
+void displayData(double length, double width, double area) {
+    cout << "rectangle data:" << endl;
+    cout << "Length: " << length << endl;
+    cout << "width: " << width << endl;
+    cout << "area: " << area << endl;
+
+}
